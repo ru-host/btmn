e171ff18cb87e511d1d5d06831ac9e90d5f1c04d
VHS69 <endusr@yandex.ru>
2020-10-19 00:40:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="keywords" content="" />
         <meta name="description" content="" />
-    	<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="favicon.png" />
-    	<link rel="icon" type="image/png" href="favicon.png" />
-    	<link rel="apple-touch-icon" href="favicon.png">
-	<link rel="stylesheet" media="all" href="style.css"  />
+    	<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/img/favicon.png" />
+    	<link rel="icon" type="image/png" href="/img/favicon.png" />
+    	<link rel="apple-touch-icon" href="/img/favicon.png">
+	<link rel="stylesheet" media="all" href="/css/style.css"  />
 </head>
 <body style="margin: 0; background: #fff; overflow: hidden;">
   <h1 style="display:block;font-family:system; font-size:18px; color:black;margin: 8px 0 16px 16px; ">app7347272</h1> 
