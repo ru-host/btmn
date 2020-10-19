b6632948cfa3c401a660914d4d918ec1c26f779b
VHS69 <endusr@yandex.ru>
2020-10-19 09:05:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,16 +12,21 @@
 </head>
 <body id="body.main" style="display: block;border: 4px solid #dae2ea;width: 808px;height: 608px;margin: 0;padding: 0;background: #fff;overflow: hidden;">
 <header>
-  <h1 id="h1.main" style="display: block;font-family: Arial;font-size: 12px;color: #597da3;margin: 8px;">&nbsp; Error 404:&nbsp;&nbsp;Page Not Found</h1>
+  <h1 id="h1.main" style="display: block;font-family: Arial;font-size: 12px;color: #597da3;margin: 8px;">&nbsp; Deep Links Dashboard (DLD) v.0.1 BETA</h1>
 </header>
-	. . . . . . . . . .<br/>
-	. . . . . . . . . .<br/>
-	. . . . . . . . . .<br/>
-	. . . . . . . . . .<br/>
-<footer>
-	<a href="https://github.com/vhs69/btmn">Powered by GitHub</a> |
-	<a href="https://vk.com/app7347272">VK.App</a> |
-	<a href="https://btmn.ru/">by BTMN.RU</a>
+	<p>
+	[1] . . . . . . . . . . <br/>
+	[2] . . . . . . . . . . <br/>
+	[3] . . . . . . . . . . <br/>
+	[4] . . . . . . . . . . <br/>
+			   	<br/>
+	</p>
+<footer  id="footer.main" style="display: block;font-family: Tahoma;font-size: 12px;color: #597da3;margin: 8px 0 0 0;">
+	<br/>&nbsp;
+	<a href="https://github.com/vhs69/btmn"> GitHub </a> |
+	<a href="https://vk.com/app7347272"> ВКонтакте </a> |
+	<a href="https://btmn.ru/"> (c) BTMN </a>
+	<br/>
 </footer>	
 </body>
 </html>
