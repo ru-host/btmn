3c711829297f71097cc437c9bffabf5b3dce6b3f
VHS69 <endusr@yandex.ru>
2020-10-19 09:09:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,22 +10,22 @@
     	<link rel="apple-touch-icon" href="/img/favicon.png">
 	<link rel="stylesheet" media="all" href="/css/style.css"  />
 </head>
-<body id="body.main" style="display: block;border: 4px solid #dae2ea;width: 808px;height: 608px;margin: 0;padding: 0;background: #fff;overflow: hidden;">
+<body id="body.main" style="display: block;border: 4px solid #dae2ea;width: 808px;height: 608px;margin: 0;padding: 0;font-family: Tahoma;font-size: 12px;background: #fff;overflow: hidden;">
 <header>
   <h1 id="h1.main" style="display: block;font-family: Arial;font-size: 12px;color: #597da3;margin: 8px;">&nbsp; Deep Links Dashboard (DLD) v.0.1 BETA</h1>
 </header>
 	<p>
-	[1] . . . . . . . . . . <br/>
-	[2] . . . . . . . . . . <br/>
-	[3] . . . . . . . . . . <br/>
-	[4] . . . . . . . . . . <br/>
+	&nbsp;[1] . . . . . . . . . . <br/>
+	&nbsp;[2] . . . . . . . . . . <br/>
+	&nbsp;[3] . . . . . . . . . . <br/>
+	&nbsp;[4] . . . . . . . . . . <br/>
 			   	<br/>
 	</p>
 <footer  id="footer.main" style="display: block;font-family: Tahoma;font-size: 12px;color: #597da3;margin: 8px 0 0 0;">
 	<br/>&nbsp;
-	<a href="https://github.com/vhs69/btmn"> GitHub </a> |
-	<a href="https://vk.com/app7347272"> ВКонтакте </a> |
-	<a href="https://btmn.ru/"> (c) BTMN </a>
+	<a href="https://github.com/vhs69/btmn">GitHub</a>&nbsp;|&nbsp;
+	<a href="https://vk.com/app7347272"> ВКонтакте</a>&nbsp;|&nbsp;
+	<a href="https://btmn.ru/">Компания</a>&nbsp;
 	<br/>
 </footer>	
 </body>
