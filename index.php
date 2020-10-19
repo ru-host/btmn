b2db771460f5e8051e41b94f3c3dba978d6216d6
VHS69 <endusr@yandex.ru>
2020-10-19 10:27:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,18 @@
 </head>
 <body id="body.main" style="display: block;border: 1px solid #dae2ea;width: 808px;height: 608px;margin: 0;padding: 0;font-family: Tahoma;font-size: 12px;background: #fff;overflow: hidden;">
 <header>
-  <h1 id="h1.main" style="display: block;font-family: Arial;font-size: 12px;color: #597da3;margin: 8px;">&nbsp; Deep Links Dashboard (DLD) v.0.1 BETA</h1>
+  <h1 id="h1.main" style="display: block;font-family: Arial;font-size: 12px;color: #597da3;margin: 8px;">&nbsp; DeepLink</h1>
 </header>
 <br/>
-	<p id="p.main" style="display: block;font-family: Tahoma;font-size: 12px;color: #123;margin: 0;">
-&nbsp;&nbsp;&nbsp;&nbsp;[1] . . . . . . . . . . <br/>
-&nbsp;&nbsp;&nbsp;&nbsp;[2] . . . . . . . . . . <br/>
-&nbsp;&nbsp;&nbsp;&nbsp;[3] . . . . . . . . . . <br/>
-&nbsp;&nbsp;&nbsp;&nbsp;[4] . . . . . . . . . . <br/>
+	<p id="p.main" style="display: block;font-family: Tahoma;font-size: 12px;color: #456;margin: 0;">
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://vk.com/btmn_ru">ВКонтакте</a><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://youtube.com/user/kinoplan">YouTube</a><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://facebook.com/btmn.ru">Facebook</a><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://instagram.com/btmn.ru">Instagram</a><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://t.me/btmn_ru">Telegram</a><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://viber.com/btmn.ru">Viber</a><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://whatsapp.com/btmn.ru">WhatsApp</a><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href="#onemore">more</a><br/>
 	</p>
 <footer  id="footer.main" style="display:block;position:absolute;bottom:0px;height:52px;width:800px;font-family: Tahoma;font-size: 14px;color: #597da3;background: #dae2ea;margin:0;">
 	<br/>&nbsp;&nbsp;
